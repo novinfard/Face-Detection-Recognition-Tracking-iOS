0fd578488b6ddd98e26731638c4b472650c53cc1
Soheil Novinfard <novinfard@gmail.com>
2019-02-10 15:32:41 +0000

    Initial commit

---
+++ FaceRecognitionTests/FaceRecognitionTests.swift
@@ -0,0 +1,34 @@
+//
+//  FaceRecognitionTests.swift
+//  FaceRecognitionTests
+//
+//  Created by Soheil on 23/01/2019.
+//  Copyright © 2019 Novinfard. All rights reserved.
+//
+
+import XCTest
+@testable import FaceRecognition
+
+class FaceRecognitionTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Face Detection, Recognition and Tracking with Machine Learning and Vision in iOS
+
+In this iOS application, we can detect a face, recognise whose face is this and then track it via video or real-time camera in 3 separate phases.
+
+I demonestrate how this process is feasible by using in-device ML and vision facilities such as CoreML and Vision framework, and how we can can make some part of this process server-side, considering critical view including advantages and disadvantages for each of them.
+
